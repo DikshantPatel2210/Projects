d7b9da67243a2cb814d735e9cbfcab267a3cbef9
DIkshantPatel2210 <sikshantpatel2210@.com>
2024-10-27 23:02:00 +0100

    setup and requirement

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from  typing import List
+
+HYPEN_E_NOT = "-e ."
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements from a requirements file
+
+    '''
+    requirements = [ ]
+    with open(file_path, 'r') as file_obj:
+          requirements = file_obj.readlines()
+          requirements= [req.replace("\n", "")for req in requirements ]
+
+          if HYPEN_E_NOT in requirements:
+              requirements.remove(HYPEN_E_NOT)
+
+    return requirements
+
+
+setup(
+name="Projects",
+version="0.0.1",
+author="DIKSHANTPATEL",
+packages=find_packages(),
+install_requires=get_requirements("requirements.txt")
+)
